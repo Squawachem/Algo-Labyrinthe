4f9ff25709bc4d0072c78f19721c0a1b41626b73
baptiste <baptiste.beltzer@etu.unilim.fr>
2018-11-09 22:24:42 +0100

    add menu.c tested V0.1

---
+++ menu.c
@@ -0,0 +1,211 @@
+#include	"labyrinthe.h"
+
+
+void menu(labyrinthe* L)
+{
+	char			ctemp;
+	char			quit;
+	position 		pos_dragon;
+	position 		pos_knight;
+	position 		pos_lab;
+	position 		pos_oratoire;
+	int 			micro_delay;
+
+	init_displayer();
+
+
+	pos_dragon.x 	= -40;
+	pos_dragon.y 	= 1;
+	pos_knight.x 	= 100;
+	pos_knight.y	= 1;
+	pos_lab.x 		= 19;
+	pos_lab.y 		= 5;
+	pos_oratoire.x 	= 40;
+	pos_oratoire.y 	= 5;
+
+/*
+			pos_dragon.x 	= -20;
+			pos_dragon.y 	= 1;
+			pos_knight.x 	= 100;
+			pos_knight.y	= 1;
+			pos_lab.x 		= 19;
+			pos_lab.y 		= 5;
+			pos_oratoire.x 	= 40;
+			pos_oratoire.y 	= 5;
+*/
+
+
+
+	micro_delay = 50000;
+	quit = 0;
+
+
+	while(quit != 1)
+	{
+		// refresh
+		menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+
+
+
+		system("/bin/stty raw");
+		ctemp = getc(stdin);
+		//fflush(stdin);
+		system("/bin/stty cooked");
+
+
+		switch (ctemp)
+		{
+		    case 27: // ESC
+		    	printf("\rVoulez vous quitter ? (y/n) ");
+		    	scanf("%1c",&ctemp);
+		    	if (ctemp=='y')
+		        	quit = 1;
+		        break;
+		    case 'e': // créer via l'éditeur
+
+		        break;
+		    case 'm': // modifier via l'éditeur
+
+		        break;
+		    case 'a': // créer aléatoirement
+
+		    	break;
+		    case 'l': // charger depuis un fichier
+
+		        break;
+		    case 's': // sauvegarder dans un fichier
+
+		        break;
+		    case 'x': // analyser le labyrinthe
+
+		        break;
+		    case 'd': // dragon
+		    	printf("\r \r");
+		    	system("/bin/stty raw");
+				ctemp = getc(stdin);
+				printf("\r \r");
+				if (ctemp=='r')
+				{
+					ctemp = getc(stdin);
+					printf("\r \r");
+					if (ctemp=='a')
+					{
+						ctemp = getc(stdin);
+						printf("\r \r");
+						if (ctemp=='g')
+						{
+							ctemp = getc(stdin);
+							printf("\r \r");
+							if (ctemp=='o')
+							{
+								ctemp = getc(stdin);
+								//printf("\r \r");
+								if (ctemp=='n')
+								{
+									system("/bin/stty cooked");
+									//déplacement des paterns
+									for(int i=0;i<18;i++)
+									{
+										pos_dragon.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+									}
+									for(int i=0;i<3;i++)
+									{
+										pos_dragon.x++;
+										pos_lab.x++;
+										pos_oratoire.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+										pos_dragon.x++;
+										pos_lab.x++;
+										pos_oratoire.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+										pos_dragon.x++;
+										pos_lab.x++;
+										pos_lab.y--;
+										pos_oratoire.x++;
+										pos_oratoire.y++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+									}
+									for(int i=0;i<4;i++)
+									{
+										pos_dragon.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+										pos_dragon.x++;
+										pos_lab.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+										pos_dragon.x++;
+										pos_lab.x++;
+										pos_oratoire.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+									}
+									for(int i=0;i<2;i++)
+									{
+										pos_dragon.x++;
+										pos_lab.x++;
+										pos_oratoire.x++;
+										menu_display(*L,pos_dragon,pos_knight,pos_lab,pos_oratoire);
+										usleep(micro_delay);
+									}
+									//positions finals atteintes
+								}
+							}
+						}
+					}
+				}
+				system("/bin/stty cooked");
+	        	break;
+		    case 'k': // knight
+		    	printf("\r \r");
+		    	system("/bin/stty raw");
+				ctemp = getc(stdin);
+				printf("\r \r");
+				if (ctemp=='n')
+				{
+					ctemp = getc(stdin);
+					printf("\r \r");
+					if (ctemp=='i')
+					{
+						ctemp = getc(stdin);
+						printf("\r \r");
+						if (ctemp=='g')
+						{
+							ctemp = getc(stdin);
+							printf("\r \r");
+							if (ctemp=='h')
+							{
+								ctemp = getc(stdin);
+								//printf("\r \r");
+								if (ctemp=='t')
+								{
+									system("/bin/stty cooked");
+									pos_dragon.x 	= -40;
+									pos_dragon.y 	= 1;
+									pos_knight.x 	= 100;
+									pos_knight.y	= 1;
+									pos_lab.x 		= 19;
+									pos_lab.y 		= 5;
+									pos_oratoire.x 	= 40;
+									pos_oratoire.y 	= 5;
+								}
+							}
+						}
+					}
+				}
+				system("/bin/stty cooked");
+		   		break;
+			default:
+				break;
+		}
+	}
+}
+
+
+
+
